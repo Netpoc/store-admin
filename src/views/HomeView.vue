3653911e6322d085180dd6da63d0d67336b30e43
Netpoc <ayenotopeter@hotmail.com>
2023-04-17 21:59:19 +0000

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,279 @@
+<template>
+  <v-app id="inspire">
+    <SideBar />
+    <v-container>
+      <v-app-bar color="rgba(0,0,0,0)" flat>
+        <v-text-field 
+        label="Search..." 
+        class="pt-5" 
+        filled 
+        prepend-inner-icon="mdi-magnify" 
+        dense solo flat 
+        background-color="grey lighten-4" 
+        rounded></v-text-field>
+        <v-spacer></v-spacer>
+        <v-btn icon dark>
+          <v-icon color="black">far fa-bell</v-icon>
+        </v-btn>
+        <v-select class="mt-7" :items="items" v-model="select" item-text="state" color="black" solo
+        flat style="max-width: 7%" item-color="grey darken-2"></v-select>
+        <span text color="#114611">Business-X</span>
+        <v-avatar size="30" class="ml-2">
+          <v-img src="https://cdn.vuetifyjs.com/images/lists/2.jpg"></v-img>
+        </v-avatar>
+      </v-app-bar>
+      <v-row>
+        <v-col cols="12" sm="8">
+        <v-card class="mx-4 rounded-xl pa-4" color="grey lighten-3" flat>
+          <v-row>
+            <v-col cols="12" sm="8">
+              <v-list-item three-line>
+                <v-list-item-content>
+                <v-list-item-title class="text-h4 mb-1">Hey Business-X. </v-list-item-title>
+                <v-list-item-subtitle>
+                  Welcome back
+                </v-list-item-subtitle>
+              </v-list-item-content>
+              </v-list-item>
+              <v-card-actions>
+                <v-btn class="px-3" color="black" dark>View Details</v-btn>
+              </v-card-actions>
+            </v-col>
+            <v-col cols="12" sm="4">
+              <v-img src="" class="mt-n16"></v-img>
+            </v-col>
+          </v-row>
+        </v-card>
+        <v-toolbar flat color="rgba(0,0,0,0)" dense class="mt-n1">
+          <v-toolbar-title>Trending Blogs</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <span class="grey--text">
+            View All
+          </span>
+        </v-toolbar>
+        <v-row class="mt-n5">
+          <v-col cols="12" sm="4">
+            <v-card class="rounded-xl ml-4" color="grey lighten-3" flat>
+              <v-card-title>
+                <span class="text-h6 font-weight-light">Prototyping</span>
+              </v-card-title>
+              <v-card-text>
+                A prototye is an early <br> sample, model release of a product...
+              </v-card-text>
+              <v-card-actions class="mt-n7">
+                <v-list-item>
+                  <div color="grey darken-3">
+                    <v-avatar class="ml-n3" size="30">
+                      <img src="https://cdn.vuetifyjs.com/images/lists/1.jpg">
+                    </v-avatar>
+                    <v-avatar class="ml-n3" size="30">
+                      <img src="https://cdn.vuetifyjs.com/images/lists/3.jpg">
+                    </v-avatar>
+                    <v-avatar class="ml-n3" size="30">
+                      <img src="https://cdn.vuetifyjs.com/images/lists/4.jpg">
+                    </v-avatar>
+                    <v-avatar class="ml-n3" size="30">
+                      <img src="https://cdn.vuetifyjs.com/images/lists/2.jpg">
+                    </v-avatar>
+                    <v-avatar class="ml-n3" size="30" color="black">
+                      <span class="caption white--text">+1</span>
+                    </v-avatar>
+                  </div>
+                  <v-row align="center" justify="center">
+                    <v-icon class="mr-1" small>fas fa-hands-wash</v-icon>
+                    <span class="caption">1.5k</span>
+                  </v-row>
+                </v-list-item>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="4">
+            <v-card class="rounded-xl ml-4" dark color="black lighten-3" flat>
+              <v-card-title>
+                <span class="text-h6 font-weight-light">Sales</span>
+              </v-card-title>
+              <v-card-text>
+                A prototye is an early <br> sample, model release of a product...
+              </v-card-text>
+              <v-card-actions class="mt-n7">
+                <v-list-item>
+                  <div color="grey darken-3">
+                    <v-avatar class="ml-n3" size="30">
+                      <img src="https://cdn.vuetifyjs.com/images/lists/1.jpg">
+                    </v-avatar>
+                    <v-avatar class="ml-n3" size="30">
+                      <img src="https://cdn.vuetifyjs.com/images/lists/3.jpg">
+                    </v-avatar>
+                    <v-avatar class="ml-n3" size="30">
+                      <img src="https://cdn.vuetifyjs.com/images/lists/4.jpg">
+                    </v-avatar>
+                    <v-avatar class="ml-n3" size="30">
+                      <img src="https://cdn.vuetifyjs.com/images/lists/2.jpg">
+                    </v-avatar>
+                    <v-avatar class="ml-n3" size="30" color="black">
+                      <span class="caption white--text">+1</span>
+                    </v-avatar>
+                  </div>
+                  <v-row align="center" justify="center">
+                    <v-icon class="mr-1" small>fas fa-hands-wash</v-icon>
+                    <span class="caption">1.5k</span>
+                  </v-row>
+                </v-list-item>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="4">
+            <v-card class="rounded-xl ml-4" color="grey lighten-3" flat>
+              <v-card-title>
+                <span class="text-h6 font-weight-light">Prototyping</span>
+              </v-card-title>
+              <v-card-text>
+                A prototye is an early <br> sample, model release of a product...
+              </v-card-text>
+              <v-card-actions class="mt-n7">
+                <v-list-item>
+                  <div color="grey darken-3">
+                    <v-avatar class="ml-n3" size="30">
+                      <img src="https://cdn.vuetifyjs.com/images/lists/1.jpg">
+                    </v-avatar>
+                    <v-avatar class="ml-n3" size="30">
+                      <img src="https://cdn.vuetifyjs.com/images/lists/3.jpg">
+                    </v-avatar>
+                    <v-avatar class="ml-n3" size="30">
+                      <img src="https://cdn.vuetifyjs.com/images/lists/4.jpg">
+                    </v-avatar>
+                    <v-avatar class="ml-n3" size="30">
+                      <img src="https://cdn.vuetifyjs.com/images/lists/2.jpg">
+                    </v-avatar>
+                    <v-avatar class="ml-n3" size="30" color="black">
+                      <span class="caption white--text">+1</span>
+                    </v-avatar>
+                  </div>
+                  <v-row align="center" justify="center">
+                    <v-icon class="mr-1" small>fas fa-hands-wash</v-icon>
+                    <span class="caption">1.5k</span>
+                  </v-row>
+                </v-list-item>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-card class="mx-4 rounded-xl pa-6 mt-2" color="grey-lighten-3" flat>
+          <v-toolbar flat color="rgba(0,0,0,0)" dense class="mt-n5">
+            <v-toolbar-title>Analytics</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn-toggle v-model="text" small dense background-color="primary" dark>
+              <v-btn value="weekly">
+                Weekly
+              </v-btn>
+              <v-btn value="monthly">
+                Monthly
+              </v-btn>
+            </v-btn-toggle>
+          </v-toolbar>
+          <v-sparkline :value="value" :smooth="radius || false"
+          :padding="padding"
+          :line-width="width"
+          :stroke-linecap="lineCap"
+          :fill="fill"
+          :type="type"
+          :auto-line-width="autoLineWidth" auto-draw color="black" class="mt-n8 mb-n8"></v-sparkline>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="4">
+        <v-card class="rounded-xl pa-4" color="grey lighten-3" flat>
+          <v-app-bar color="(0,0,0,0)" tile>
+            <v-btn tile fab small color="orange lighten-4" elevation="0">
+              <v-icon color="black">fas fa-book</v-icon>
+            </v-btn>
+            <strong class="subtitle black--text ml-2">
+              Articles Posted <span class="caption"><br>80</span>
+            </strong>
+            <v-spacer></v-spacer>
+            <v-icon color="grey" class="ml-2">fas fa-chevron-circle-right</v-icon>
+          </v-app-bar>
+
+          <v-app-bar color="(0,0,0,0)" tile class="mt-3">
+            <v-btn tile fab small color="green lighten-4" elevation="0">
+              <v-icon color="black">fas fa-hands-wash</v-icon>
+            </v-btn>
+            <strong class="subtitle black--text ml-2">
+              Claps This Week <span class="caption"><br>1.5k</span>
+            </strong>
+            <v-spacer></v-spacer>
+            <v-icon color="grey" class="ml-2">fas fa-chevron-circle-right</v-icon>
+          </v-app-bar>
+
+          <v-app-bar color="(0,0,0,0)" tile class="mt-3">
+            <v-btn tile fab small color="blue lighten-4" elevation="0">
+              <v-icon color="black">fas fa-user-friends</v-icon>
+            </v-btn>
+            <strong class="subtitle black--text ml-2">
+              Claps This Week <span class="caption"><br>1.5k</span>
+            </strong>
+            <v-spacer></v-spacer>
+            <v-icon color="grey" class="ml-2">fas fa-chevron-circle-right</v-icon>
+          </v-app-bar>
+          <v-toolbar flat color="rgba(0,0,0,0)" dense class="mt-2">
+            <v-toolbar-title>Followers</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <span class="grey--text">View All</span>
+          </v-toolbar>
+          <div class="pw-7">
+            <vueper-slides :infinite="false" class="no-shadow"
+            :visible-slides="3"
+            slide-multiple
+            :gap="3"
+            :slide-ratio="1 / 4"
+            :dragging-distance="200"
+            :breakpoints="{800: {visibleSlide: 2, slideMultiple: 2}}">
+          <template #arrow-left>
+            <v-icon class="ml-n15">fas fa-chevron-left</v-icon>
+          </template>
+          <template #arrow-right>
+            <v-icon class="ml-n15">fas fa-chevron-right</v-icon>
+          </template>
+          <vueper-slide v-for="(slide, i) in slides" :key="i" :image="slide.src" />
+        </vueper-slides>
+          </div>
+        </v-card>
+        
+      </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
+  
+</template>
+
+<script>
+import SideBar from '@/components/SideBar.vue';
+import {VueperSlides, VueperSlide} from 'vueperslides'
+ export default {
+    name: 'Home',
+    data: () => ({
+      select: {state: 'EN'},
+      items: ['EN', 'ES','DZ'],
+      text: 'weekly',
+      width:2,
+      radius: 10,
+      padding: 8,
+      lineCap: 'round',
+      value: [0,2,5,9,5,10,3,5,0,0,1,8,2,9,0],
+      fill: false,
+      type: 'trend',
+      autoLineWidth: false,
+      slides: [
+        {src: require('../assets/logo.png')},
+        {src: require('../assets/logo.png')},
+        {src: require('../assets/logo.png')},
+        {src: require('../assets/logo.png')},
+        {src: require('../assets/logo.png')}
+      ]
+    }),
+    components: {
+      SideBar,
+      VueperSlides,
+      VueperSlide,
+    },
+  }
+</script>
